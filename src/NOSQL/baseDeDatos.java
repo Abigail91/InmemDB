aeb53427a2361e7410cc4a6f9a42507e26d8fd3d
meji1026 <48700410+meji1026@users.noreply.github.com>
2019-05-22 23:36:18 -0600

    Update baseDeDatos.java

--- src/NOSQL/baseDeDatos.java
+++ src/NOSQL/baseDeDatos.java
@@ -6,6 +6,13 @@ import java.util.Hashtable;
 import estructurasDeDatos.ListaEnlazadaSimple;
 
 public class baseDeDatos {
+			  		/**
+ * Esta clase se encarga del manejo de la base de datos
+ * @author: Abigail Abarca, Haziel Gudino, Daniel Nunez, Victoria Ruiz
+ 
+ * @version: 5/22/2019
+ 
+ */
 	
 	public HashTable<String, ListaEnlazadaSimple<HashTable>> tablas;
 	
